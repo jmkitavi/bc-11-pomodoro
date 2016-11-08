cda3e8ace7e8a85691bd09aa2b21d8171162ce9b
sirjmkitavi <kitavimjoseph@gmail.com>
2016-11-08 15:43:18 +0300

    Editing my code to PEP8 standards

--- config.py
+++ config.py
@@ -1,35 +1,35 @@
-#task time settings
+#set task time
 def set_time():
     task_duration = raw_input("Enter time for task as HH:MM:SS").split(':')
     try:
-        for i in [0, 1, 2]:		#check if all 3 fields are present h:m:s
+        for i in [0, 1, 2]:     #check if all 3 fields are present h:m:s
             task_duration[i] = int(task_duration[i])
         return task_duration
     except:
         print "The Input wasn't Valid!"
         set_time()
 
-#short break settings
-def shortbreak_settings():
+#set short breaks
+def short_break_settings():
     short_break = raw_input("Enter time for short break as HH:MM:SS").split(':')
     try:
-        for i in [0, 1, 2]:		#check/get all 3 fields if present h:m:s
+        for i in [0, 1, 2]:     #check/get all 3 fields if present h:m:s
             short_break[i] = int(short_break[i])
         return short_break
     except:
         print "The Input wasn't Valid!"
-        shortbreak_settings()
+        short_break_settings()
 
 #long break settings
-def longbreak_settings():
-    long_break = raw_input("Enter time for short break as HH:MM:SS").split(':')
+def long_break_settings():
+    long_break = raw_input("Enter time for long break as HH:MM:SS").split(':')
     try:
         for i in [0, 1, 2]:  # check/get all 3 fields if present h:m:s
             long_break[i] = int(long_break[i])
         return long_break
     except:
         print "The Input wasn't Valid!"
-        longbreak_settings()
+        long_break_settings()
 
 #sound seting
 def sound_settings():
--- countdown_timer.py
+++ countdown_timer.py
@@ -1,74 +1,72 @@
-#!/usr/bin/env python
+# #!/usr/bin/env python
 import pygame
 
+
+# func to convert time to seconds
+def time_in_sec(time):
+    # time in hh:mm:ss
+    total_time = time.split(':')
+
+    try:  # check/get all 3 fields if present hh:mm:ss
+        for i in [0, 1, 2]:
+            total_time[i] = int(total_time[i])
+    except:
+        print "The Input wasn't Valid!"
+        quit()
+
+    # convert time to seconds
+    # [2] is sec + [1] min*60 + [0] hrs*60*60
+    time_in_seconds = (total_time[2]) + (total_time[1] * 60) + (total_time[0] * (60 * 60))
+    return time_in_seconds
+
+
 def countdown(time, title):
-	# initializing pygame
-	# title = "cycle 1"
-	pygame.init()
-	timer_width = 280;
-	timer_height = 120
-	timer_dsp = pygame.display.set_mode((timer_width, timer_height))
-	pygame.display.set_caption("Pomodoro Timer: "+title)
-	Font = pygame.font.SysFont('monospace', 15)
+    pygame.init()
+    timer_width = 280
+    timer_height = 120
+    timer_dsp = pygame.display.set_mode((timer_width, timer_height))
+    pygame.display.set_caption("Pomodoro: " + title)
+    font = pygame.font.SysFont('monospace', 15)
 
-	#time for timer in h min sec
-	TotalTime = time.split(':')
+    total_time_in_seconds = time_in_sec(time)
 
-	# convert time to integer
-	try:
-	    for i in [0, 1, 2]:		#check if all 3 fields are present h:m:s
-	        TotalTime[i] = int(TotalTime[i])
-	except:
-	    print "The Input wasn't Valid!"
-	    quit()
-
-	# conver time to seconds
-	#[2] is sec + [1] min*60 + [0] hrs*60*60
-	TotalTimeSeconds = (TotalTime[2]) + (TotalTime[1] * 60) + (TotalTime[0] * (60*60))
-
-
-	#formatting countdown output
-	def countdown_time(Time_In_Seconds):
-	    Hours = Time_In_Seconds / (60 ** 2)
-	    Minutes = (Time_In_Seconds / 60) % 60
-	    Seconds = Time_In_Seconds % 60
-	    return "%i H, %i Min and %i Sec" % (Hours, Minutes, Seconds)
-
-
-	# Wait for the user's input to start the countdown
-	# raw_input("Press [Enter] to begin the countdown!\n")
+    # formatting countdown output
+    def countdown_time(time):
+        hours = time / (60 ** 2)
+        minutes = (time / 60) % 60
+        seconds = time % 60
+        return "%i H, %i Min and %i Sec" % (hours, minutes, seconds)
 
 
 
-	#function to quit pygame timer
-	def quit_timer():
-	    Events = pygame.event.get()
-	    for Event in Events:
-	        if Event.type == pygame.QUIT or (Event.type == pygame.KEYDOWN and Event.key == pygame.K_ESCAPE):
-	            pygame.quit()
-	            quit()
+    # function to quit pygame timer on key in event
+    def quit_timer():
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                pygame.quit()
+                print "Timer terminated."
+                quit()
 
-	# Run the Timer
-	while TotalTimeSeconds > 0:
-	    quit_timer()
-	    # pause_timer()
-	    if TotalTimeSeconds <= 30:
-	        timer_dsp.fill((255, 4.25 * TotalTimeSeconds, 4.25 * TotalTimeSeconds))
-	    else:
-	        timer_dsp.fill((255, 255, 255))
-	    String = countdown_time(TotalTimeSeconds)
-	    Font_Position = (
-	        (0.5 * timer_width) - (0.5 * Font.size(String)[0]),
-	        (0.5 * timer_height) - (0.5 * Font.size(String)[1])
-	    )
-	    Rendered_Font = Font.render(String, 1, (0, 0, 0))
-	    timer_dsp.blit(Rendered_Font, Font_Position)
-	    pygame.display.flip()
-	    pygame.time.wait(1000)
-	    TotalTimeSeconds -= 1
+    # running the Timer
+    while total_time_in_seconds > 0:
+        quit_timer()
+        if total_time_in_seconds <= 30:
+            timer_dsp.fill((255, 4.25 * total_time_in_seconds, 4.25 * total_time_in_seconds))
+        else:
+            timer_dsp.fill((255, 255, 255))
+        string = countdown_time(total_time_in_seconds)
+        font_position = (
+            (0.5 * timer_width) - (0.5 * font.size(string)[0]),
+            (0.5 * timer_height) - (0.5 * font.size(string)[1])
+        )
+        rendered_font = font.render(string, 1, (0, 0, 0))
+        timer_dsp.blit(rendered_font, font_position)
+        pygame.display.flip()
+        pygame.time.wait(1000)
+        total_time_in_seconds -= 1
+
+    pygame.quit()
 
 
-
-	pygame.quit()
-	print "The Countdown Finished!"
-countdown("00:01:15","Cycle 1")
+countdown("00:01:15", "Cycle 1")
